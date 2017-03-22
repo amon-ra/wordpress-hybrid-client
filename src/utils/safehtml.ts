ce0028c3c878484a9ee00c86d624917b94e7c540
AmonRa <amon.raj@gmail.com>
2017-03-22 11:39:53 +0100

    added filters and css filter configs

--- src/utils/safehtml.ts
+++ src/utils/safehtml.ts
@@ -1,10 +1,25 @@
 import {DomSanitizer} from '@angular/platform-browser';
 import {PipeTransform, Pipe} from "@angular/core";
+import { Config } from '../providers/config';
 
 @Pipe({ name: 'safeHtml'})
 export class SafeHtmlPipe implements PipeTransform  {
-  constructor(private sanitized: DomSanitizer) {}
-  transform(value) {
-    return this.sanitized.bypassSecurityTrustHtml(value);
+  constructor(private config: Config, private sanitized: DomSanitizer) {}
+  transform(value: any, enable = false,filters = [] as string[]) {
+    console.log('safehtml');
+    console.log(enable);
+    console.log(filters);
+    console.log(value);
+    for(let regex of filters){
+      value=value.replace(regex);
+    }
+    for(let regex of this.config.getApi('filters')){
+      value=value.replace(regex);
+    }
+    console.log(value);
+    if (this.config.getApi('safehtml') == true || enable)
+      return this.sanitized.bypassSecurityTrustHtml(value);
+    else
+      return value;
   }
 }
